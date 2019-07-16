1ad0906e5008a0745b044113e2db6dc21d8f68de
orhanayd <info@orhanaydogdu.com.tr>
2019-07-16 12:22:18 +0300

    v2.4.1
    cumhuriyet 2. servis linki eklendi.

--- funcs/cumhuriyet.php
+++ funcs/cumhuriyet.php
@@ -3,46 +3,68 @@
             /// Cumhuriyet BAŞLANGIÇ ////
             function get_cumhuriyet(){
                 $desc="İstediğiniz artık yok veya hatalı işlem.";
+                $curl_status=false;
                 $catNews=null;
                 $status=false;
                 $file=curl_function($_ENV["get_cumhuriyet"]); // Tüm manşetler
                 if($file['status']){
-                        $desc="from agency";
-                        $status=true;
-                        foreach($file['result'] as $raw){
-                            // cumhuriyet unix date paylaşmadığı için biz de kendimiz üretiyoruz :)
-                            $unix=intval(preg_replace('/\D/', '', parse_url($raw['coverUrl'])['query'])/1000);
-                            $date=gmdate("d.m.Y H:i:s", $unix);
-                            $news_title=$raw['title'];
-                            $news_summary=$raw['summary'];
-                            $new_id=(int)$raw['id'];
-                            $catNews[]=array(
-                                "agency"=>"cumhuriyet",
-                                "agency_title"=>"Cumhuriyet",
-                                "categories"=>getCategorie("cumhuriyet", $raw['categoryName']),
-                                "id"=>$new_id,
-                                "date"=>$date,
-                                "date_u"=>$unix,
-                                "title"=>$news_title,
-                                "seo_link"=>seolink($news_title, "cumhuriyet", $new_id),
-                                "spot"=>$news_summary,
-                                "image"=>"https://images.ulak.news/?src="."http://cumhuriyet.com.tr".$raw['coverUrl'],
-                                "url"=>"http://cumhuriyet.com.tr".$raw['newsUrl']
-                            );
-                    };
+                    $curl_status=true;
                 }else{
-                    $desc="Cumhuriyet ile bağlantı kurulamadı. api@orhanaydogdu.com.tr";
+                    $file=curl_function($_ENV["get_cumhuriyet2"]); // Tüm manşetler
+                    if($file['status']){
+                        $file=$file['result'];
+                        $curl_status=true;
+                    }else{
+                        $desc="Cumhuriyet ile bağlantı kurulamadı. 2. api@orhanaydogdu.com.tr";
+                    }
+                }
+                if($curl_status){
+                    $desc="from agency";
+                    $status=true;
+                    foreach($file['result'] as $raw){
+                        // cumhuriyet unix date paylaşmadığı için biz de kendimiz üretiyoruz :)
+                        $unix=intval(preg_replace('/\D/', '', parse_url($raw['coverUrl'])['query'])/1000);
+                        $date=gmdate("d.m.Y H:i:s", $unix);
+                        $news_title=$raw['title'];
+                        $news_summary=$raw['summary'];
+                        $new_id=(int)$raw['id'];
+                        $catNews[]=array(
+                            "agency"=>"cumhuriyet",
+                            "agency_title"=>"Cumhuriyet",
+                            "categories"=>getCategorie("cumhuriyet", $raw['categoryName']),
+                            "id"=>$new_id,
+                            "date"=>$date,
+                            "date_u"=>$unix,
+                            "title"=>$news_title,
+                            "seo_link"=>seolink($news_title, "cumhuriyet", $new_id),
+                            "spot"=>$news_summary,
+                            "image"=>"https://images.ulak.news/?src="."http://cumhuriyet.com.tr".$raw['coverUrl'],
+                            "url"=>"http://cumhuriyet.com.tr".$raw['newsUrl']
+                        );
+                    };
                 }
                 return array("status"=>$status, "result"=>$catNews, "desc"=>$desc);
             }
 
             function get_cumhuriyet_new($new_id){
                 global $agency, $allowed_tags;
+                $curl_status=false;
                 $status=false;
                 $result=null;
                 $desc="İstediğiniz artık yok veya hatalı işlem.";
                 $file=curl_function($_ENV["get_cumhuriyet_new"]."?newsId=".$new_id);
                 if($file['status']){
+                    $curl_status=true;
+                }else{
+                    $file=curl_function($_ENV["get_cumhuriyet2"]."?newsId=".$new_id);
+                    if($file['status']){
+                        $file=$file['result'];
+                        $curl_status=true;
+                    }else{
+                        $desc="Cumhuriyet ile bağlantı kurulamadı. 2. api@orhanaydogdu.com.tr";
+                    }
+                }
+                if($curl_status){
                     $news=$file['result'];
                     if($news[0]['id']!=null){
                         $desc="from agency";
@@ -82,8 +104,6 @@
                             saveDatabase($agency, $result);
                         }
                     }
-                }else{
-                    $desc="Cumhuriyet ile bağlantı kurulamadı. api@orhanaydogdu.com.tr";
                 }
                 return array("status"=>$status, "result"=>$result, "desc"=>$desc);
             }
